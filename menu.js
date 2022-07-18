384db723ccb8aac3a5e5a5fa94e82efb94b7576b
Vasyl Cherkashyn <panvas22@gmail.com>
2022-07-18 13:56:56 +0300

    1111

--- menu.js
+++ menu.js
@@ -1,16 +1,14 @@
-// (() => {
-//   const refs = {
-//     openMenuBtn: document.querySelector(".menu-open-btn"),
-//     closeMenuBtn: document.querySelector(".mobile-menu__close"),
-//     menu: document.querySelector(".mobile-menu"),
-//     body: document.querySelector("body"),
-//   };
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-btn]");
+  const mobileMenuRef = document.querySelector("[data-menu-container]");
 
-//   refs.openMenuBtn.addEventListener("click", toggleMenu);
-//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-//   function toggleMenu() {
-//     refs.menu.classList.toggle("is-hidden");
-//     refs.body.classList.toggle("no-scroll");
-//   }
-// })();
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
